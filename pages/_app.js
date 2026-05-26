3238357c4bfbe25a1cf023b9f80d7a8349e7fe3f
njr-research <research@njrpartners.com>
2026-05-26 22:50:02 +1000

    icon

--- pages/_app.js
+++ pages/_app.js
@@ -35,6 +35,7 @@ export default function App({ Component, pageProps }) {
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
         <meta name="theme-color" content="#0a0a0f" />
+        <link rel="apple-touch-icon" href="/icon.png" />
       </Head>
       <div style={{ minHeight: '100vh', background: '#0a0a0f', color: '#e2e8f0', fontFamily: 'system-ui, sans-serif' }}>
         <NavBar />
